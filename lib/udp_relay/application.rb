6bec917fac67bf27740651e3d581b46aa57c2b13
João Bruno <jnnlbruno@gmail.com>
2022-09-07 07:58:18 +0100

    get_message was not at the appropriate level: fixed

--- lib/udp_relay/application.rb
+++ lib/udp_relay/application.rb
@@ -33,33 +33,33 @@ module UdpRelay
         socket.close
       end
     end
-  end
 
-  def get_message sender, sentence, source_id
-    if !source_id.nil?
-      if sentence.chars.count { |x| x == "\\" } >= 2
-        # already has block
+    def get_message sender, sentence, source_id
+      if !source_id.nil?
+        if sentence.chars.count { |x| x == "\\" } >= 2
+          # already has block
 
-        if sentence.include? "s:"
-          # replace
-          tokens = sentence.split("s:", 2)
-          rest_of_sentence = tokens[1].partition(/[,*]/).drop(1).join
+          if sentence.include? "s:"
+            # replace
+            tokens = sentence.split("s:", 2)
+            rest_of_sentence = tokens[1].partition(/[,*]/).drop(1).join
 
-          return "[#{sender[3]}]#{tokens[0]}s:#{source_id}#{rest_of_sentence}"
+            return "[#{sender[3]}]#{tokens[0]}s:#{source_id}#{rest_of_sentence}"
+          else
+            # introduce inside block
+            tokens = sentence.split("\\")
+            existing_block_contents = tokens[1]
+            rest_of_sentence = tokens[2]
+
+            return "[#{sender[3]}]\\s:#{source_id},#{existing_block_contents}\\#{rest_of_sentence}"
+          end
         else
-          # introduce inside block
-          tokens = sentence.split("\\")
-          existing_block_contents = tokens[1]
-          rest_of_sentence = tokens[2]
-
-          return "[#{sender[3]}]\\s:#{source_id},#{existing_block_contents}\\#{rest_of_sentence}"
+          # introduce new block with s:
+          return "[#{sender[3]}]\\s:#{source_id}*00\\#{sentence}"
         end
-      else
-        # introduce new block with s:
-        return "[#{sender[3]}]\\s:#{source_id}*00\\#{sentence}"
       end
-    end
 
-    "[#{sender[3]}]#{sentence}"
+      "[#{sender[3]}]#{sentence}"
+    end
   end
 end
